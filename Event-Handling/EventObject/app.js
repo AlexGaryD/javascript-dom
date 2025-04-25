edaa251a7aa81985e6be35dc572d90238b2547ae
Alexis Vera S. <alxvcs93@hotmail.com>
2025-04-25 18:53:57 -0500

    Coloque los comentarios a la clase Eventos en JavaScript: Accediendo y Manipulando el DOM

--- Event-Handling/EventObject/app.js
+++ Event-Handling/EventObject/app.js
@@ -1,9 +1,22 @@
+// Seleccionar el botón en el DOM
 const button = document.querySelector("button");
+// Selecciona el primer elemento <button> que aparece en el documento HTML.
 
+// Definir una función de callback para manejar el evento "click"
 const buttonClicked = (event) => {
+  // `event` es el objeto del evento que contiene información sobre el evento que ocurrió.
+
   console.log(event.target);
+  // Muestra en la consola el elemento HTML que disparó el evento (en este caso, el botón).
+
   console.log(event.target.id);
+  // Muestra el atributo `id` del elemento que disparó el evento.
+  // Si el botón no tiene un `id`, imprimirá `undefined`.
+
   console.log(event.target.textContent);
+  // Muestra el texto contenido dentro del botón que disparó el evento.
 };
 
+// Añadir un evento "click" al botón
 button.addEventListener("click", buttonClicked);
+// Escucha el evento "click" en el botón y ejecuta la función `buttonClicked` cuando ocurre.
